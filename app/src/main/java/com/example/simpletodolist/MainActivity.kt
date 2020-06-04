ff927cc22677d842aa2f06f974e9067175f265ff
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-06-04 22:57:30 +0300

    keyboard fix

--- app/src/main/java/com/example/simpletodolist/MainActivity.kt
+++ app/src/main/java/com/example/simpletodolist/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.simpletodolist
 
-import android.app.Activity
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wiseassblog.jetpacknotesmvvmkotlin.model.RoomNoteDatabase
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,29 +13,21 @@ import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val db=RoomNoteDatabase.getInstance(this)
-        runBlocking {  recycler_view.adapter=TaskAdapter(db.roomNoteDao().getTasks()) }
+        runBlocking { recycler_view.adapter=TaskAdapter(db.roomNoteDao().getTasks()) }
         recycler_view.layoutManager=LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
-        recycler_view.setOnTouchListener(object:View.OnTouchListener {
 
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                    hideSoftKeyboard()
-                return true
-            }
-        })
-    }
-
-
-    fun Activity.hideSoftKeyboard() {
-        currentFocus?.let {
-            val inputMethodManager = ContextCompat.getSystemService(this, InputMethodManager::class.java)!!
-            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+        recycler_view.setOnTouchListener { v, _ ->
+            val imm=
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(v.windowToken, 0)
+            false
         }
     }
-
 }
