077e2418d1efca561f030e08fcbe7da06f28cece
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-06-04 22:45:56 +0300

    keyboard work

--- app/src/main/java/com/example/simpletodolist/MainActivity.kt
+++ app/src/main/java/com/example/simpletodolist/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.simpletodolist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.wiseassblog.jetpacknotesmvvmkotlin.model.RoomNoteDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.runBlocking
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +24,22 @@ class MainActivity : AppCompatActivity() {
         runBlocking {  recycler_view.adapter=TaskAdapter(db.roomNoteDao().getTasks()) }
         recycler_view.layoutManager=LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
+        recycler_view.setOnTouchListener(this)
+        recycler_view.setOnTouchListener(object:View.OnTouchListener {
+
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                    hideSoftKeyboard()
+                return true
+            }
+        })
     }
+
+
+    fun Activity.hideSoftKeyboard() {
+        currentFocus?.let {
+            val inputMethodManager = ContextCompat.getSystemService(this, InputMethodManager::class.java)!!
+            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+        }
+    }
+
 }
