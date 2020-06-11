d216d9b287c2824d17b664825429d49c7b821da2
kenny kurochkin <kennnkuro15@gmail.com>
2020-06-11 20:12:29 +0300

    work

--- app/src/main/java/com/example/simpletodolist/model/RandomColors.kt
+++ app/src/main/java/com/example/simpletodolist/model/RandomColors.kt
@@ -3,9 +3,10 @@ package com.example.simpletodolist.model
 class RandomColors {
 
     private val colors = listOf(
-        0xff1a1334,
-        0xff26294a,
-        0xff01545a,
+        0xff40E0D0,
+        0xff50C878,
+        0xff3399cc,
+
         0xff017351,
         0xff03c383,
         0xffaad962,
