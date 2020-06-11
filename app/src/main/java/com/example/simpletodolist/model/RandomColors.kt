c3e3f1a0092d429197bc29accdab1e0626448ba2
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-06-11 04:55:26 +0300

    work

--- app/src/main/java/com/example/simpletodolist/model/RandomColors.kt
+++ app/src/main/java/com/example/simpletodolist/model/RandomColors.kt
@@ -2,17 +2,22 @@ package com.example.simpletodolist.model
 
 class RandomColors {
 
-    private var colors = arrayOf(0xfffec6262,0xe1d276,0xff99ea86,0xff83d6b7,
-        0xffe4a8f9,0xffcaf1de,0xffccffcc,
-        0xff00bcd4,
-        
-
-        0xff009688,0xff4caf50,0xff8bc34a,0xffcddc39,
-        0xffffeb3b,0xffffc107,0xffff9800,0xffff5722,
-        0xff795548,0xff9e9e9e,0xff607d8b,0xff333333)
-
+    private val colors = listOf(
+        0xff1a1334,
+        0xff26294a,
+        0xff01545a,
+        0xff017351,
+        0xff03c383,
+        0xffaad962,
+        0xfffbbf45,
+        0xffef6a32,
+        0xffed0345,
+        0xffa12a5e,
+        0xff710162,
+        0xff022c7d
+    )
 
     fun getRandomColor() : Long {
-        return colors.asList().shuffled()[0]
+        return colors.random()
     }
 }
