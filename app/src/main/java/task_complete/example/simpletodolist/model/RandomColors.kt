93804f9a82dc43a5376e5314d753888a67b40e0e
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-06-14 21:30:33 +0300

    changed colors

--- app/src/main/java/task_complete/example/simpletodolist/model/RandomColors.kt
+++ app/src/main/java/task_complete/example/simpletodolist/model/RandomColors.kt
@@ -3,25 +3,17 @@ package task_complete.example.simpletodolist.model
 class RandomColors {
 
     private val colors = listOf(
-        0xff40E0D0,
-        0xff50C878,
-        0xff3399cc,
-
-        0xff017351,
-        0xff03c383,
-        0xffaad962,
-        0xfffbbf45,
-        0xffef6a32,
-        0xffed0345,
-        0xffa12a5e,
-        0xff710162,
-        0xff022c7d
-
+        0xffEA6153,
+        0xffFD982F,
+        0xffFAD741,
+        0xff3AB37C,
+        0xff3D6591,
+        0xff9677A7
     )
     private var colorsList =colors.toMutableList()
 
     fun getRandomColor() : Long {
-        if (colorsList.size == 2) {
+        if (colorsList.size == 1) {
             colorsList = colors.toMutableList()
         }
         val color = colorsList.random()
